021eff86bbce7facdbbe273db4b4034a93a9ce3d
Soroka_Volodymyr <soroka.volodymyr@knu.ua>
2026-02-15 17:28:51 +0200

    java// made some first steps

--- app.js
+++ app.js
@@ -1,1 +1,190 @@
+const theme1 = document.getElementById('theme');
+const status1 = document.getElementById('status');
+const priority1 = document.getElementById('priority');
+const comment1 = document.getElementById('comment');
+const username1 = document.getElementById('username');
+const createForm = document.getElementById('createForm');
+const resetBtn = document.getElementById('resetBtn');
+const tbody = document.getElementById('itemstablebody');
 
+let items = [];
+function rendertable(items) {
+
+    const rowsHtml = items.map((item, index) => `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${item.theme}</td>
+            <td>${item.status}</td>
+            <td>${item.priority}</td>
+            <td><div class ="scroll-wrapper"> ${item.comment}</div></td>
+            <td>${item.username}</td>
+            <td>
+                <button type="button" class="delete-btn" data-id="${item.id}"> Видалити </button>
+            </td>
+        </tr>
+        `).join('');
+
+        tbody.innerHTML = rowsHtml;
+}
+
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+
+        const theme = theme1.value.trim();
+        const status = status1.value;
+        const priority = priority1.value;
+        const comment = comment1.value.trim();
+        const username = username1.value.trim();
+
+
+    if (!theme || !status || !priority || !comment || !username) {
+        alert('Будь ласка, заповніть усі поля');
+        return;
+    }
+    const newItem = {
+        id: Date.now().toString(),
+        theme,
+        status,
+        priority,
+        comment,
+        username,
+    };
+
+    console.log('Новий елемент', newItem);
+    items.push(newItem);
+    rendertable(items);
+    createForm.reset();
+});
+
+
+resetBtn.addEventListener('click', (e) => {
+    createForm.reset();
+})
+
+
+tbody.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-btn')) {
+        const id = e.target.getAttribute('data-id');
+        console.log('Видаляємо ID рядок:', id, typeof id);
+        if (confirm('Видалити цю заявку?')) {
+            items = items.filter(item => {
+                console.log('Порівнюємо:', item.id, typeof item.id, 'з', id, typeof id);
+                return item.id !== id
+            });
+            console.log('Масив після видалення:', items);
+            rendertable(items);
+        }
+    }
+});
+
+rendertable(items);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*tbody.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-btn')) {
+        const id = parseInt(e.target.getAttribute('data-id'));
+        items = items.filter((item) => item.id === id);
+        rendertable(items);
+    }
+});
+
+rendertable(items);
+
+
+
+
+/*
+console.log(theme);
+
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const dto = readForm();
+    const isValid = validate(dto);
+    if (!isValid) return;
+
+    addItem(dto);
+    rendertable(items);
+    resetForm();
+});
+
+documet.getElementById("resetBtn").addEventListener("click", () => {
+    resetForm();
+    clearErrors();
+});
+
+function validate(dto) {
+    clearErrors();
+    let isValid = true;
+    // перевірки...
+    // якщо помилка:
+    // showError(...); isValid = false;
+    return isValid;
+}
+
+function showError(inputId, errorId, message) {
+    document.getElementById(inputId).classList.add("invalid");
+    document.getElementById(errorId).innerHTML = message;
+}
+
+
+if (dto.status === "") {
+    showError("statusSelect", "statusError", "Оберіть значення зі списку.");
+    isValid = false;
+}
+
+
+
+
+ */
