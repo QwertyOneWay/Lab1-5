96b1d709e10fadddd3c19ecdd9b175fa7a746241
Soroka_Volodymyr <soroka.volodymyr@knu.ua>
2026-03-04 18:50:15 +0200

    add files state.js and ui.js

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-const createForm = document.getElementById('createForm');
-const resetBtn = document.getElementById('resetBtn');
-const tbody = document.getElementById('itemstablebody');
-
-let items = JSON.parse(localStorage.getItem("items")) || [];
-let editingId = null;
-function saveToLocalStorage() {
-    localStorage.setItem("items", JSON.stringify(items));
-}
-
-function clearError(inputId, errorId) {
-    document.getElementById(inputId).classList.remove("invalid");
-    document.getElementById(errorId).innerHTML = "";
-}
-
-function clearErrors() {
-    clearError("theme", "theme-error");
-    clearError("status", "status-error");
-    clearError("priority", "priority-error");
-    clearError("comment", "comment-error");
-    clearError("username", "username-error");
-}
-
-function showError(inputId, errorId, message) {
-    document.getElementById(inputId).classList.add('invalid');
-    document.getElementById(errorId).innerHTML = message;
-}
-
 function Validate(dto) {
     clearErrors()
 
@@ -94,42 +66,6 @@ function readForm(){
         username: document.getElementById("username").value,
         createdAt: new Date().toISOString(),
     };
-}
-
-function fillForm(dto){
-    document.getElementById("theme").value = dto.theme;
-    document.getElementById("priority").value = dto.priority;
-    document.getElementById("comment").value = dto.comment;
-    document.getElementById("username").value = dto.username;
-    document.getElementById("status").value = dto.status;
-}
-
-function clearForm(){
-    document.getElementById("theme").value = "";
-    document.getElementById("status").value = "";
-    document.getElementById("priority").value = "";
-    document.getElementById("comment").value = "";
-    document.getElementById("username").value = "";
-}
-
-function rendertable(items) {
-    const rowsHtml = items.map((item, index) => `
-        <tr>
-            <td>${index + 1}</td>
-            <td>${item.theme}</td>
-            <td>${item.status}</td>
-            <td>${item.priority}</td>
-            <td><div class ="scroll-wrapper"> ${item.comment}</div></td>
-            <td>${item.username}</td>
-            <td>
-                <div class="table-buttons-style">
-                    <button type="button" class="delete-btn" data-id="${item.id}"> Видалити </button>
-                    <button type="button" class="edit-btn" data-id="${item.id}"> Редагувати </button>
-                </div>
-            </td>
-        </tr>
-        `).join('');
-    tbody.innerHTML = rowsHtml;
 }
 
 createForm.addEventListener('submit', (e) => {
